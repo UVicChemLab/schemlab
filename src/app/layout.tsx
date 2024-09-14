57420e2f111d5691241218292505db521c0bf53e
bhans-uvic <bhans@uvic.ca>
2024-09-14 11:10:16 -0700

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Spectroscopy Chemistry Lab",
+  description: "An open-source interactive spectroscopy website",
+  icons: [{ rel: "icon", url: "/compound.png" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
